75cb9128416de4cf5ba57b5487efef1a058f86e9
Thiago Gomes <theago.dev@gmail.com>
2026-07-04 14:31:55 -0300

    [SSE] - Adding sse simple example.

---
+++ server-side-event/example/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const port = 8080;
+const cors = require("cors")
+// const {createClient} = require("redis")
+
+
+const app = express()
+app.use(cors());
+
+app.get('/stream', async (req, res) => {
+  res.header("content-type", "text/event-stream"); // header obrigatorio para SSE
+  res.header("connection", "keep-alive"); // somente para HTTP antigo como 1.0, para falar para manter a con alive
+
+    setInterval(() => {
+    res.write("data: " + Math.random() + "\n\n"); // "data: " e "\n\n" sao obrigatorios para cada msg
+  }, 3000);
+
+  // const redisClient = createClient({"url": "redis://redis:6379"});
+  // redisClient.on('error', err => console.log('Redis Client Error', err));
+
+  // await redisClient.connect();
+
+  // await redisClient.subscribe("notifications", (message) => {
+  //   res.write("data: " + message + "\n\n");
+  // })
+
+  // No redis-cli : PUBLISH notifications "Minha primeira mensagem!"
+});
+
+app.listen(port, () => {});
